41150e1741faae468a6756dcd9a71c5d008479d5
netlink-support <112404379+netlink-support@users.noreply.github.com>
2025-07-01 18:19:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -336,7 +336,217 @@ document.addEventListener("DOMContentLoaded", (function () {
 
 const Us = function () {
     var t = document.querySelector(".ind_map"),
-        e = [/* city data */];
+        e = [{
+                    x: 12.1,
+                    y: 46.5,
+                    content: "Ahmedabad"
+                },
+                {
+                    x: 20.9,
+                    y: 13,
+                    content: "Poonch"
+                },
+                {
+                    x: 23.5,
+                    y: 21,
+                    content: "Ludhiana"
+                },
+                {
+                    x: 17.5,
+                    y: 25,
+                    content: "Sri Ganganagar"
+                },
+                {
+                    x: 27.5,
+                    y: 28,
+                    content: "Delhi"
+                },
+                {
+                    x: 28.8,
+                    y: 30,
+                    content: "Ghaziabad"
+                },
+                {
+                    x: 28.9,
+                    y: 32,
+                    content: "Mathura"
+                },
+                {
+                    x: 14.9,
+                    y: 30,
+                    content: "Bikaner"
+                },
+                {
+                    x: 22.9,
+                    y: 34,
+                    content: "Jaipur"
+                },
+                {
+                    x: 20.9,
+                    y: 37,
+                    content: "Ajmer"
+                },
+                {
+                    x: 14.9,
+                    y: 36,
+                    content: "Jodhpur"
+                },
+                {
+                    x: 30.5,
+                    y: 37,
+                    content: "Gwalior"
+                },
+                {
+                    x: 23.9,
+                    y: 39,
+                    content: "Kota"
+                },
+                {
+                    x: 16.5,
+                    y: 42,
+                    content: "Udaipur"
+                },
+                {
+                    x: 22.9,
+                    y: 47,
+                    content: "Ujjain"
+                },
+                {
+                    x: 28.5,
+                    y: 46,
+                    content: "Bhopal"
+                },
+                {
+                    x: 34.2,
+                    y: 46,
+                    content: "Sagar"
+                },
+                {
+                    x: 37.2,
+                    y: 47,
+                    content: "Jabalpur"
+                },
+                {
+                    x: 44.2,
+                    y: 50,
+                    content: "Bilaspur"
+                },
+                {
+                    x: 23,
+                    y: 49,
+                    content: "Indor"
+                },
+                {
+                    x: 14.6,
+                    y: 50,
+                    content: "Vadodara"
+                },
+                {
+                    x: 13.6,
+                    y: 53,
+                    content: "Surat"
+                },
+                {
+                    x: 10.6,
+                    y: 52,
+                    content: "Bhavnagar"
+                },
+                {
+                    x: 7.9,
+                    y: 50,
+                    content: "Rajkot"
+                },
+                {
+                    x: 13.9,
+                    y: 55,
+                    content: "Vapi"
+                },
+                {
+                    x: 16.9,
+                    y: 57,
+                    content: "Nasik"
+                },
+                {
+                    x: 21.9,
+                    y: 58,
+                    content: "Aurangabad"
+                },
+                {
+                    x: 26.9,
+                    y: 55,
+                    content: "Akola"
+                },
+                {
+                    x: 28.9,
+                    y: 54,
+                    content: "Amravati"
+                },
+                {
+                    x: 33.9,
+                    y: 54,
+                    content: "Nagpur"
+                },
+                {
+                    x: 43.5,
+                    y: 54,
+                    content: "Raipur"
+                },
+                {
+                    x: 42.9,
+                    y: 60,
+                    content: "Jagdalpura"
+                },
+                {
+                    x: 32.5,
+                    y: 67,
+                    content: "Hyderabad"
+                },
+                {
+                    x: 13.5,
+                    y: 60,
+                    content: "Mumbai"
+                },
+                {
+                    x: 16.5,
+                    y: 64,
+                    content: "Pune"
+                },
+                {
+                    x: 16.9,
+                    y: 69,
+                    content: "Kolhapur"
+                },
+                {
+                    x: 28.2,
+                    y: 81,
+                    content: "Bengaluru"
+                },
+                {
+                    x: 37.2,
+                    y: 82,
+                    content: "Chennai"
+                },
+                {
+                    x: 35.8,
+                    y: 86,
+                    content: "Puducherry"
+                },
+                {
+                    x: 33,
+                    y: 90,
+                    content: "Tiruchirappalli"
+                },
+                {
+                    x: 29.9,
+                    y: 93,
+                    content: "Madurai"
+                },
+                {
+                    x: 26.7,
+                    y: 98,
+                    content: "Thiruvanathapuram"
+                },
+            ];
 
     function i() {
         var t = document.querySelectorAll(".hot-spot"),
